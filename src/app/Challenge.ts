117d40cb5dd239fcba4eb7c2ada13505749a29cb
Wilmar De MelquisedecLisbet <carewaz23@hotmail.com>
2021-09-21 17:23:33 -0500

    ok ejercicio transformArray

--- src/app/Challenge.ts
+++ src/app/Challenge.ts
@@ -48,6 +48,26 @@ class Challenge {
 
         return response
     }
+
+    public async transformArray(matriz: any[][], order: string){
+        let response: number[] = []
+
+        for (let i = 0; i < matriz.length; i++) {
+            for (let j = 0; j < matriz[i].length; j++) {
+                const parse = Number(matriz[i][j])
+                !isNaN(parse) && parse % 1 === 0 && response.push(parse)            
+            }
+            
+        }
+        
+        if (order) {
+            order.toUpperCase() === 'ASC' ? response.sort((a, b) => a - b) :
+            order.toUpperCase() === 'DESC' ? response.sort((a, b) => b - a) :
+            response
+        }
+
+        return response
+    }
 }
 
 export default Challenge
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express'
 import average from './average'
 import stringManipulation from './stringManipulation'
 import sumArray from './sumArray'
+import transformArray from './transformArray'
 
 const router = Router()
 
 router.use('/average', average)
 router.use('/strManipulation', stringManipulation)
 router.use('/sumArray', sumArray)
+router.use('/transformArray', transformArray)
 
 export default router
